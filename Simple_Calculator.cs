bd63bfcf82f93c371e18a039ac7f2e599b90fa9e
Sonya Power <113512496+Sonya-ass@users.noreply.github.com>
2022-09-14 13:08:16 +0300

    Update Simple_Calculator.cs

--- Simple_Calculator.cs
+++ Simple_Calculator.cs
@@ -6,18 +6,21 @@ namespace Basic_Calculator
     {
         static void Main(string[] args)
         {
-           
+            //ввод первого числа
             Console.WriteLine("Enter a number: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
 
+            //ввод второго числа
             Console.WriteLine("Enter another number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
+            //ввод операции
             Console.WriteLine("Enter operation(+, -, *, /): ");
 
             string conv = Console.ReadLine();
             //Console.WriteLine(conv);
 
+            //выполнение операций
             if(conv.Equals("+"))
             {
                 Console.Write(num1 + num2);
